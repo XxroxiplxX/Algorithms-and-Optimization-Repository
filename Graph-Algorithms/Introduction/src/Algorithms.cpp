beabb9c0de494c6d27f725542e866673c9e3a27a
piotrkali <261696@student.pwr.edu.pl>
2023-03-29 08:09:15 -0400

    graph algorithms finished

--- Graph-Algorithms/Introduction/src/Algorithms.cpp
+++ Graph-Algorithms/Introduction/src/Algorithms.cpp
@@ -64,7 +64,7 @@ bool topoDFSVisit(Vertex* source, DirectedGraph* dg, std::list<std::pair<int,int
             //v->parent = source;
             //std :: cout << "visiting vertex with id: " << v->id << std::endl;
             v->parent_id = source->id;
-            std::cout << source->id << " is now parent of " << v->id << std::endl;
+            //std::cout << source->id << " is now parent of " << v->id << std::endl;
             topoDFSVisit(v, dg, sortedList);
         }
     }
@@ -129,21 +129,44 @@ void IDFSvisit(Vertex* source, DirectedGraph* dg) {
         }
     }
 }
-void sortedDFS(DirectedGraph* dg, std::list<std::pair<int,int>> sortedVertices) {
+int iDFSvisit(Vertex* source, AbstractGraph* g, int c) {
+    //source->color = 'b';
+    g->getVertex(source->id)->color = 'g';
+    g->time++;
+    int d=0;
+    g->getVertex(source->id)->d = g->time;
+    for (auto v : source->neighbours) {
+        if (v->color == 'w') {
+            //v->parent = source;
+            std :: cout << "visiting vertex with id: " << v->id << std::endl;
+            v->parent_id = source->id;
+            //std::cout << source->id << " is now parent of " << v->id << std::endl;
+            d=iDFSvisit(v, g, c);
+        }
+    }
+
+    g->getVertex(source->id)->color = 'b';
+    g->time++;
+    g->getVertex(source->id)->f = g->time;
+    return c+d+1;
+}
+void sortedDFS(DirectedGraph* dg, std::list<std::pair<int,int>> sortedVertices, std::list<std::pair<Vertex*,int>>& heads) {
     //std::stack<int> s;
     for (auto item : sortedVertices) {
         //std::cout << "[" << item.first << "   " << item.second << "]" << " --> ";
         if (dg->getVertex(item.first)->color == 'w') {
             //s.push(item.first);
-            //DFSvisit(dg->getVertex(item.first), dg);
+            //std::cout << item.first << std::endl;
 
+            int s = iDFSvisit(dg->getVertex(item.first), dg, 0);
+            heads.push_front(std::pair<Vertex*, int>(dg->getVertex(item.first), s));
         }
     }
 }
-DirectedGraph* stronglyConnectedComponents(DirectedGraph* dg) {
+DirectedGraph* stronglyConnectedComponents(DirectedGraph* dg, std::list<std::pair<Vertex*,int>>& heads) {
     auto transposedDirectedGraph = dg->transpose();
     auto sortedVerticesList = topologicalSort(dg);
-    sortedDFS(transposedDirectedGraph, sortedVerticesList);
+    sortedDFS(transposedDirectedGraph, sortedVerticesList, heads);
     return transposedDirectedGraph;
 }
 void DFSTreeVisit(Vertex* source, DirectedGraph* dg) {
@@ -194,4 +217,45 @@ bool isBipartite(AbstractGraph* ag) {
         }
     }
     return  true;
+}
+void modBFS(AbstractGraph* ag) {
+    /*for (int i = 0; i < ag->vertices.size(); i++) {
+        if (ag->vertices[i].color == 'w') {
+            std::cout << "vertex " << ag->vertices[i].id << std::endl;
+            auto source = ag->getVertex(i+1);
+            //source->color = 'g';
+            std::queue<Vertex*> queue;
+            queue.push(source);
+            //log->log(queue.front()->id);
+            //std::cout << "\n" << queue.front()->id << std::endl;
+            source->color = 'r';
+            while (!queue.empty()) {
+                auto u = queue.front();
+                //u->color = 62 + currColor;
+                //std::cout << u->id << "Upokolorowano na: " << u->color << std::endl;
+                currColor *= -1;
+                //std :: cout << "visiting vertex with id: " << u->id << std::endl;
+                for (auto v : u->neighbours) {
+                    if (v->color == 'w') {
+                        //v->color = u->color + currColor;
+                        if (u->color == 'r') {
+                            v->color = 'b';
+                        } else {
+                            v->color = 'r';
+                        }
+                        //std::cout << v->id <<  "Vpokolorowano na: " << v->color << std::endl;
+                        currColor *= -1;
+                        //u->neighbours.push_back(v);
+                        //v->parent_id = u->id;
+                        queue.push(v);
+                    } else if (v->color == u->color) {
+                        //std::cout << "wierzcholki " << v->id << " i " << u->id << std::endl;
+                        return false;
+                    }
+                }
+                queue.pop();
+                //u->color = 'b';
+            }
+        }
+    }*/
 }
--- Graph-Algorithms/Introduction/src/Algorithms.h
+++ Graph-Algorithms/Introduction/src/Algorithms.h
@@ -15,12 +15,13 @@ void DFSvisit(Vertex* source, AbstractGraph* g);
 void printPath(Vertex* source, Vertex* destination);
 std::list<std::pair<int, int>> topologicalSort(DirectedGraph* dg);
 bool topoDFSVisit(Vertex* source, DirectedGraph* dg, std::list<std::pair<int, int>>& sortedList);
-void sortedDFS(DirectedGraph* dg, std::list<std::pair<int,int>> sortedVertices);
-DirectedGraph* stronglyConnectedComponents(DirectedGraph* dg);
+void sortedDFS(DirectedGraph* dg, std::list<std::pair<int,int>> sortedVertices, std::list<std::pair<Vertex*,int>>& heads);
+DirectedGraph* stronglyConnectedComponents(DirectedGraph* dg, std::list<std::pair<Vertex*,int>>& heads);
 void DFSTreeVisit(Vertex* source, DirectedGraph* dg);
 void printForest(DirectedGraph* dg);
 void IDFSvisit(Vertex* source, DirectedGraph* dg);
 bool isBipartite(AbstractGraph* ag);
+void modBFS(AbstractGraph* ag);
 //94855167282784
 //47239281841184
 
