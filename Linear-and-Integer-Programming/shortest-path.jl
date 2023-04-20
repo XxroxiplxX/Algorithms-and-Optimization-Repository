b4de71e453d643bd5a5a982a1996ccbe9165e608
piotrkali <261696@student.pwr.edu.pl>
2023-04-20 11:31:18 +0200

    comsetic changes

--- Linear-and-Integer-Programming/shortest-path.jl
+++ Linear-and-Integer-Programming/shortest-path.jl
@@ -6,14 +6,15 @@ n = 7#size(costs[1])
 shortest_path = Model(GLPK.Optimizer)
 source = 3
 dest = 7
-
-@variable(shortest_path, route[1:n,1:n], Int)
+T = 1000000
+@variable(shortest_path, route[1:n,1:n], Bin)
 
 @constraint(shortest_path, [i = 1:n, j = 1:n; times[i,j] == 0], route[i,j] == 0)
 @constraint(shortest_path, [i = 1:n, j = 1:n; i != source && i != dest], sum(route[i,:]) == sum(route[:,i]))
 @constraint(shortest_path, sum(route[source,:]) - sum(route[:,source]) == 1)
 @constraint(shortest_path, sum(route[dest,:]) - sum(route[:,dest]) == -1)
-@objective(shortest_path, Min, dot(times, route))
+constraint(model, sum(route[i,j] for i in 1:n, j in 1:n) <= T)
+@objective(shortest_path, Min, sum(route[i,j]*costs[i,j] for i in 1:n, j in 1:n))
 optimize!(shortest_path)
 @show objective_value(route)
 @show value.(route)
