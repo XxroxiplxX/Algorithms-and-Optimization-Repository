9411100a7a5ebaf128f9cfeeb71929cae82300f0
piotrkali <261696@student.pwr.edu.pl>
2023-05-06 20:44:21 +0200

    aaaa

--- cppconf.sh
+++ cppconf.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
-mkdir -p $1/{build,lib,src,tst}
-cd $1
-echo "
+if [ $1 = "--info" ]
+then
+    echo "1-nazwa projektu, 2-lokalizacja projektu"
+else
+    cd $2
+    mkdir -p $1/{build,src,tst}
+    cd $1
+    echo "
 cmake_minimum_required(VERSION 3.10)
 project($1)
 
-set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD 14)
 
 include_directories(src)
 
 add_subdirectory(src)
 add_subdirectory(tst)
-add_subdirectory(lib/googletest)
+add_subdirectory(googletest)
 " > CMakeLists.txt
-cd src
-echo 'set(BINARY ${CMAKE_PROJECT_NAME})
+    cd src
+    echo 'set(BINARY ${CMAKE_PROJECT_NAME})
 
 file(GLOB_RECURSE SOURCES LIST_DIRECTORIES true *.h *.cpp)
 
@@ -24,13 +29,13 @@ add_executable(${BINARY}_run ${SOURCES})
 
 add_library(${BINARY}_lib STATIC ${SOURCES})
 ' > CMakeLists.txt
-echo "#include <iostream>
+    echo "#include <iostream>
 int main() {
 return 0;
 }" > main.cpp
-cd ..
-cd tst
-echo 'set(BINARY ${CMAKE_PROJECT_NAME}_tst)
+    cd ..
+    cd tst
+    echo 'set(BINARY ${CMAKE_PROJECT_NAME}_tst)
 
 file(GLOB_RECURSE TEST_SOURCES LIST_DIRECTORIES false *.h *.cpp)
 
@@ -41,15 +46,14 @@ add_executable(${BINARY} ${TEST_SOURCES})
 add_test(NAME ${BINARY} COMMAND ${BINARY})
 
 target_link_libraries(${BINARY} PUBLIC ${CMAKE_PROJECT_NAME}_lib gtest)' > CMakeLists.txt
-echo '#include "gtest/gtest.h"
+    echo '#include "gtest/gtest.h"
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }' > main.cpp
-cd ..
-cd lib
-git clone https://github.com/google/googletest/
-cd ..
-echo "project $1 initialized under directory: " pwd
+    cd ..
+    ln -s /usr/lib/googletest
+
 cd build
+fi
