d33e21b380a37ccea3b4ce11fd6d59872c27bb7f
piotrkali <261696@student.pwr.edu.pl>
2023-05-06 21:00:49 +0200

    renaming cuz windows

--- Pathfinding/A_star/build/src/CMakeFiles/A_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libA_lib.a"
-)
--- Pathfinding/A_star/cmake-build-debug/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/piotrkali/projects/Algorithms-and-Optimization-Repository/Pathfinding/A*/googletest/googlemock
-# Build directory: /home/piotrkali/projects/Algorithms-and-Optimization-Repository/Pathfinding/A*/cmake-build-debug/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
