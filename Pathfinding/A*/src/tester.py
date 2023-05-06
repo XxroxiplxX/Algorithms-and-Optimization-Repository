9245cccab8bceeddae73cc54304761357554d50a
piotrkali <261696@student.pwr.edu.pl>
2023-05-06 20:50:35 +0200

    renaming cuz windows

--- Pathfinding/A*/src/tester.py
+++
@@ -1,7 +0,0 @@
-from os import system
-heuristic = "improved"
-for i in range(10):
-    k=21
-    while (k < 26):
-        system("bash launcher.sh " + heuristic + " " + str(k) + " ../stats/" + heuristic + str(k) + ".csv")
-        k+=1
