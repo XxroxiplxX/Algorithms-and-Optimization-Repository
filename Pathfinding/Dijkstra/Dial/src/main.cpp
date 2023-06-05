8c4d9b0c04b71aeacade4542241bb5ab7b879fbf
korycki <261696@student.pwr.edu.pl>
2023-06-05 20:40:57 +0200

    bug fixed B)

--- Pathfinding/Dijkstra/Dial/src/main.cpp
+++ Pathfinding/Dijkstra/Dial/src/main.cpp
@@ -7,7 +7,7 @@ int main() {
 
     std::cout << "graph built successfully with highest cost: " << graph->get_highest_cost() << std::endl;
     int ok;
-    std::cin>>ok;
+    //std::cin>>ok;
     dijkstra_to_all_nodes(graph->get_vertex(951313), *graph);
     delete parser;
     delete graph;
